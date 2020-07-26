eeab3db122ae0a088d40834c23d13b6f1d60ee31
Ash <valleyknight@protonmail.com>
2020-07-26 16:22:20 -0400

    fix indentation

--- sxhkdrc-mode.el
+++ sxhkdrc-mode.el
@@ -1,11 +1,11 @@
 (setq sxhkdrc-highlights
-      '(("^.*#.*$" . font-lock-comment-face)
-		("    .*$" . font-lock-function-name-face)
-        ("[a-zA-Z0-9]\\|_" . font-lock-constant-face)
-		("+\\|;\\|{\\|,\\|}" . font-lock-string-face)))
+     '(("^.*#.*$" . font-lock-comment-face)
+       ("    .*$" . font-lock-function-name-face)
+       ("[a-zA-Z0-9]\\|_" . font-lock-constant-face)
+       ("+\\|;\\|{\\|,\\|}" . font-lock-string-face)))
 
 (define-derived-mode sxhkdrc-mode fundamental-mode "sxhkdrc"
-  "major mode for syntax highlighting of sxhkdrc."
-  (setq font-lock-defaults '(sxhkdrc-highlights)))
+     "major mode for syntax highlighting of sxhkdrc."
+     (setq font-lock-defaults '(sxhkdrc-highlights)))
 
 (add-to-list 'auto-mode-alist '("sxhkdrc" . sxhkdrc-mode))
